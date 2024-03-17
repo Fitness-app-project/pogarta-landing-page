c0992f9e99bdbbb17d20dc50e4b069b3fffa66ea
NiceLeader <maciek2002.10@wp.pl>
2024-03-17 03:57:25 +0100

    Add FooterBar component

---
+++ src/components/FooterBar/FooterBar.tsx
@@ -0,0 +1,10 @@
+export const FooterBar = () => {
+    return (
+        <div className="relative bg-[#383838] h-10  tracking-widest w-dvw">
+      <p className="text-white text-align: center absolute top-2  left-1/2 transform -translate-x-1/2 text-base ">
+        © 2024 - POGARTA, All rights Reserved.
+      </p>
+    </div>
+    )
+}
+    
