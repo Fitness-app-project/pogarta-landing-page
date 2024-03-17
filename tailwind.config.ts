c33f237c1b331897aa77270aea02a98d77dc0981
NiceLeader <maciek2002.10@wp.pl>
2024-03-17 15:52:19 +0100

    Add height utility class '142' to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      height: {
+        '142': '35.5rem',
+            },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
