a979f71df133489b158736b859d14323c668beda
NiceLeader <maciek2002.10@wp.pl>
2024-03-17 16:15:38 +0100

    Update Screen1 component styling

--- src/components/Screen1/Screen1.tsx
+++ src/components/Screen1/Screen1.tsx
@@ -3,7 +3,7 @@
 export const Screen1 = () => {
 
     return (
-      <div className="w-full h-142	 flex flex-col justify-center  items-center  text-[#D9B55E] ">
+      <div className="w-full h-screen1	flex flex-col justify-center  items-center  text-[#D9B55E] ">
         <h1 className="text-9xl	font-cinzel mb-5">
           Sic Parvis Magna
         </h1>
