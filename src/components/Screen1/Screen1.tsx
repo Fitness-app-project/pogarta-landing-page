c96daad38538999624ce415b834dbaef85074143
NiceLeader <maciek2002.10@wp.pl>
2024-03-17 15:48:04 +0100

    Refactor Screen1 component

--- src/components/Screen1/Screen1.tsx
+++ src/components/Screen1/Screen1.tsx
@@ -1,22 +1,15 @@
-import Image from "next/image"
-import PhonePng from '/public/images/Phone.png'
+
 
 export const Screen1 = () => {
 
     return (
-        <div className="w-full h-full flex justify-between items-center mt-24">
-            <div className="w-1/2 flex flex-col justify-center gap-10">
-                <h1 className="uppercase text-4xl text-gray-600 font-bold">Jesteśmy by <span className="text-lime-500">tworzyć</span></h1>
-                <span className="text-gray-500 font-medium tracking-wider text-lg">Realizujemy pomysł począwszy od projektu poprzez <br /> kolejne procesy produkcji i wprowadzenie na rynek.</span>
-                {/*<button className="btn w-[220px] bg-lime-500 text-white uppercase tracking-widest rounded-3xl">Dowiedz się więcej</button>*/}
-            </div>
-            <div className="w-1/2 flex justify-center items-center">
-                <Image
-                    src={PhonePng}
-                    alt="Phone"
-                    style={{ transform: "scale(1)", translate: "0%" }}
-                />
-            </div>
-        </div>
-    )
-}
+      <div className="w-full h-142	 flex flex-col justify-center  items-center  text-[#D9B55E] ">
+        <h1 className="text-9xl	font-cinzel mb-5">
+          Sic Parvis Magna
+        </h1>
+        <p className=" text-lg  tracking-widest text-[#747474]">
+          From small beginnings come great things
+        </p>
+      </div>
+    );
+};
