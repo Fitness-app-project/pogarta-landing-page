3475490b2a81ac50160814467f6700ffb5ccafef
NiceLeader <maciek2002.10@wp.pl>
2024-03-17 03:57:33 +0100

    Update offers and styling in Screen7 component

--- src/components/Screen7/Screen7.tsx
+++ src/components/Screen7/Screen7.tsx
@@ -3,13 +3,12 @@ import Image from "next/image"
 
 export const Screen7 = () => {
     const offers = [
-        { title: 'Project Manager', details: '3 040 - 4 550 USD Net/month - B2B' },
-        { title: 'Frontend Developer', details: '3 040 - 4 550 USD Net/month - B2B' },
-        { title: 'Backend Developer', details: '3 040 - 4 550 USD Net/month - B2B' },
-        // Assuming you have three more offers to include:
-        { title: 'UX/UI Designer', details: '3 040 - 4 550 USD Net/month - B2B' },
-        { title: 'QA Engineer', details: '3 040 - 4 550 USD Net/month - B2B' },
-        { title: 'DevOps Engineer', details: '3 040 - 4 550 USD Net/month - B2B' },
+        { title: 'Project Manager', details: '3 040 - 4 300 USD Net/month  B2B' },
+        { title: 'Frontend Developer', details: '4 050 - 6 070 USD Net/month  B2B' },
+        { title: 'Backend Developer', details: '5 820 - 7 590 USD Net/month  B2B' },
+        { title: 'UX/UI Designer', details: '5 060 - 5 820 USD Net/month  B2B' },
+        { title: 'QA Engineer', details: '4 820 - 6 200 USD Net/month  B2B' },
+        { title: 'Fullstack Developer', details: '6 800 - 8 070 USD Net/month  B2B' },
     ];
 
     const chunkArray = (arr: any[], chunkSize: number) => {
@@ -28,19 +27,22 @@ export const Screen7 = () => {
         {chunkedOffers.map((chunk, chunkIndex) => (
             <div key={chunkIndex} className="flex justify-center space-x-8 mb-8">
                 {chunk.map((offer, index) => (
-                    <div key={index} className="bg-black bg-opacity-10 p-10 rounded-full flex justify-between items-center w-[calc(50%-4rem)]">
-                        <div>
+                    <div key={index} className="bg-black bg-opacity-10 p-10 rounded-full flex items-center space-x-4 w-[calc(50%-4rem)] ">
+                        <div className="flex-1 ml-10">
                             <h2 className="text-2xl text-[#D9B55E]">{offer.title}</h2>
                             <p className="text-[#B7B7B7] text-lg">{offer.details}</p>
                         </div>
-                        <button className="bg-[#747474] bg-opacity-30 hover:bg-gray-600 text-[#D9B55E] py-3 px-6 rounded-full transition duration-300">
-                            KNOW MORE →
-                        </button>
+                        <div className="ml-auto"> 
+                            <button className="bg-[#747474] bg-opacity-30 hover:bg-gray-600 text-[#D9B55E] py-3 px-6 rounded-full transition duration-300 ml-5">
+                                KNOW MORE →
+                            </button>
+                        </div>
                     </div>
                 ))}
             </div>
         ))}
     </div>
+    
     
     );
 };
