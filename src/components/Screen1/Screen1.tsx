ed7fa0af080b690b48acce4365b8d073fb05a862
NiceLeader <maciek2002.10@wp.pl>
2024-03-18 01:01:23 +0100

    test animation

--- src/components/Screen1/Screen1.tsx
+++ src/components/Screen1/Screen1.tsx
@@ -1,12 +1,21 @@
 'use client'
+import { motion } from "framer-motion";
 import Image from "next/image"
-import bg from "/public/images/bg.svg"
+import bg from "/public/images/bgwhite.svg"
 import SPM from "/public/images/Sic_Parvis_Magna.svg"
 
 export const Screen1 = () => {
     return (
-      <div className="w-full h-screen1 flex flex-col justify-center items-center text-[#D9B55E] relative">
-        <Image src={bg} layout="fill" objectFit="cover" alt="background" className="z-0" />
+      <div className="w-full h-screen1 flex flex-col justify-center items-center text-[#D9B55E] ">
+        <motion.div
+          initial={{ x: '-20vw' }}
+          animate={{ x: 0 }}
+          transition={{ duration: 2 }}
+          className=" absolute z-0 w-full h-screen1 mr-24 "
+
+        >
+          <Image src={bg} layout="fill" objectFit="cover" alt="background" className="z-0 opacity-10 " />
+        </motion.div>
         <Image src={SPM} alt="Sic Parvis Magna" className="z-10 mb-5" />
         <p className="text-lg tracking-widest text-[#747474] z-10">
           From small beginnings come great things
