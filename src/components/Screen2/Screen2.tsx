87787aace61cae648b3152ce4f4ff9ba717f733c
naMqe <namqe07@gmail.com>
2024-03-19 10:39:56 +0100

    added screen2 animations

--- src/components/Screen2/Screen2.tsx
+++ src/components/Screen2/Screen2.tsx
@@ -1,3 +1,6 @@
+"use client"
+import { motion } from "framer-motion"
+
 export const Screen2 = () => {
 
     return (
@@ -6,31 +9,51 @@ export const Screen2 = () => {
 
             <div className="w-screen flex  justify-evenly  text-[#FFFFFF] bg-[#000000] bg-opacity-10 h-[230px] z-1 ">
 
-                <div className="flex inset-0 items-center gap-8 w-[280px] h-[70px] px-2 m-auto z-10  ">
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 2 }} 
+                    className="flex inset-0 items-center gap-8 w-[280px] h-[70px] px-2 m-auto z-10"
+                >
                     <span className="text-[#886A38] text-5xl font-extrabold">5+</span>
                     <span className="text-xl font-medium ">Lat doświadczenia</span>
-                </div>
+                </motion.div>
                 
                 <div className="divider lg:divider-horizontal" style={{ margin: 0 }}></div> 
 
-                <div className="flex items-center gap-8 w-[280px] h-[70px] px-2 m-auto ">
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 2 }} 
+                    className="flex inset-0 items-center gap-8 w-[280px] h-[70px] px-2 m-auto z-10"
+                >
                     <span className="text-[#8D6F3B] text-5xl font-extrabold">70+</span>
                     <span className="text-xl font-medium">Ukończonych projektów</span>
-                </div>
+                </motion.div>
 
                 <div className="divider lg:divider-horizontal" style={{ margin: 0 }}></div> 
                 
-                <div className="flex items-center gap-8 w-[280px] h-[70px] px-2 m-auto">
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 2 }} 
+                    className="flex inset-0 items-center gap-8 w-[280px] h-[70px] px-2 m-auto z-10"
+                >
                     <span className="text-[#766749] text-5xl font-extrabold">50+</span>
                     <span className="text-xl font-medium">Klientów z całego świata</span>
-                </div>
+                </motion.div>
                 
                 <div className="divider lg:divider-horizontal" style={{ margin: 0 }}></div> 
                 
-                <div className="flex items-center gap-8 w-[280px] h-[70px] px-2 m-auto">
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 2 }} 
+                    className="flex inset-0 items-center gap-8 w-[280px] h-[70px] px-2 m-auto z-10"
+                >
                     <span className="text-[#C0A56F] text-5xl font-extrabold">30+</span>
                     <span className="text-xl font-medium">Specjalistów</span>
-                </div>
+                </motion.div>
 
             </div>
         </div>
