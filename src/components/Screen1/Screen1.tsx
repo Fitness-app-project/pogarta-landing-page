ced68f232a16eb83bc77e255256228bf524740d5
NiceLeader <maciek2002.10@wp.pl>
2024-03-21 14:12:51 +0100

    Add react-countup library and implement animated background

--- src/components/Screen1/Screen1.tsx
+++ src/components/Screen1/Screen1.tsx
@@ -4,19 +4,30 @@ import Image from "next/image"
 import bg from "/public/images/bgwhite.svg"
 import SPM from "/public/images/Sic_Parvis_Magna.svg"
 
+const AnimatedBackground = ({ delay = 0 }) => (
+  <motion.div
+    initial={{ x: '-110vw' }}
+    animate={{ x: '110vw' }}
+    transition={{ duration: 15, repeat: Infinity, delay }}
+    className="absolute z-0 w-full h-screen1 mr-24 "
+  >
+    <Image src={bg} layout="fill" objectFit="cover" alt="background" className="z-0 opacity-10 " />
+  </motion.div>
+);
+
 export const Screen1 = () => {
     return (
       <div className="w-full h-screen1 flex flex-col justify-center items-center text-[#D9B55E] ">
-        <motion.div
-          initial={{ x: '-20vw' }}
-          animate={{ x: 0 }}
-          transition={{ duration: 2 }}
-          className=" absolute z-0 w-full h-screen1 mr-24 "
+        <AnimatedBackground />
+        <AnimatedBackground delay={7.5} />
 
+        <motion.div 
+          animate={{ y: ["0%", "1%", "0%"] }}
+          transition={{ yoyo:Infinity ,duration: 2, repeat: Infinity }}
+          className="z-10 mb-5"
         >
-          <Image src={bg} layout="fill" objectFit="cover" alt="background" className="z-0 opacity-10 " />
+          <Image src={SPM} alt="Sic Parvis Magna" />
         </motion.div>
-        <Image src={SPM} alt="Sic Parvis Magna" className="z-10 mb-5" />
         <p className="text-lg tracking-widest text-[#747474] z-10">
           From small beginnings come great things
         </p>
--- src/components/Screen2/Screen2.tsx
+++ src/components/Screen2/Screen2.tsx
@@ -1,60 +1,36 @@
 "use client"
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
+import CountUp from 'react-countup';
+
+const statVariants = {
+    hover: { scale: 1.05 }
+};
 
 export const Screen2 = () => {
-
     return (
         <div className="w-screen h-full flex flex-col items-center z-1">
-           
-
-            <div className="w-screen flex  justify-evenly  text-[#FFFFFF] bg-[#000000] bg-opacity-10 h-[230px] z-1 ">
-
-                <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ duration: 2 }} 
-                    className="flex inset-0 items-center gap-8 w-[280px] h-[70px] px-2 m-auto z-10"
-                >
-                    <span className="text-[#886A38] text-5xl font-extrabold">5+</span>
-                    <span className="text-xl font-medium ">Lat doświadczenia</span>
-                </motion.div>
-                
-                <div className="divider lg:divider-horizontal" style={{ margin: 0 }}></div> 
-
-                <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ duration: 2 }} 
-                    className="flex inset-0 items-center gap-8 w-[280px] h-[70px] px-2 m-auto z-10"
-                >
-                    <span className="text-[#8D6F3B] text-5xl font-extrabold">70+</span>
-                    <span className="text-xl font-medium">Ukończonych projektów</span>
-                </motion.div>
-
-                <div className="divider lg:divider-horizontal" style={{ margin: 0 }}></div> 
-                
-                <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ duration: 2 }} 
-                    className="flex inset-0 items-center gap-8 w-[280px] h-[70px] px-2 m-auto z-10"
-                >
-                    <span className="text-[#766749] text-5xl font-extrabold">50+</span>
-                    <span className="text-xl font-medium">Klientów z całego świata</span>
-                </motion.div>
-                
-                <div className="divider lg:divider-horizontal" style={{ margin: 0 }}></div> 
-                
-                <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ duration: 2 }} 
-                    className="flex inset-0 items-center gap-8 w-[280px] h-[70px] px-2 m-auto z-10"
-                >
-                    <span className="text-[#C0A56F] text-5xl font-extrabold">30+</span>
-                    <span className="text-xl font-medium">Specjalistów</span>
-                </motion.div>
-
+            <div className="w-screen flex justify-evenly text-white bg-[#000000] bg-opacity-10 h-[230px] z-1 ">
+                {[{ end: 12, color: '#886A38', text: 'Years of Experience' }, 
+                  { end: 600, color: '#8D6F3B', text: 'Completed Projects' },
+                  { end: 120, color: '#766749', text: 'Clients Worldwide' },
+                  { end: 40, color: '#C0A56F', text: 'Specialists' }].map((item, index) => (
+                    <motion.div
+                        variants={statVariants}
+                        initial={{ scale: 1 }}
+                        animate={{ scale: 1 }}
+                        whileHover="hover"
+                        custom={index}
+                        className="flex inset-0 items-center gap-8 w-[280px] h-[70px] px-2 m-auto z-10"
+                        key={index}
+                    >
+                        <span className={`text-5xl font-extrabold`} style={{ color: item.color }}>
+                            <CountUp end={item.end} duration={2.5} />+
+                        </span>
+                        <span className="text-xl font-medium">
+                            {item.text}
+                        </span>
+                    </motion.div>
+                ))}
             </div>
         </div>
     )
