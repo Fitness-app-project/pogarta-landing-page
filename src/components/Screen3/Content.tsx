acf63ee92ce3f148f699b0f99c05e9d532b94892
naMqe <namqe07@gmail.com>
2024-04-22 14:15:10 +0200

    fixed screen3 on mobile

--- src/components/Screen3/Content.tsx
+++ src/components/Screen3/Content.tsx
@@ -20,14 +20,14 @@ export const Content: React.FC<IProps> = ({ sequence, text1, text2 }) => {
                 whileInView={{ opacity: 1 }}
                 transition={{ duration: 3 }}
                 viewport={{ once: true }}
-                className='absolute left-[-120px] lg:left-[-80px] xl:left-0'
+                className='absolute left-[-140px] lg:left-[-80px] xl:left-0'
             >
                 <Image
                     alt='Coin'
                     src="/images/coin1.png"
                     width={400}
                     height={400}
-                    className="scale-50 md:scale-75 xl:scale-100"
+                    className="scale-[0.4] md:scale-75 xl:scale-100"
                 />
             </motion.div>
             <motion.div
@@ -35,7 +35,7 @@ export const Content: React.FC<IProps> = ({ sequence, text1, text2 }) => {
                 whileInView={{ opacity: 1 }}
                 transition={{ duration: 3 }}
                 viewport={{ once: true }}
-                className="pl-[130px] md:pl-[200px] lg:pl-[250px] xl:pl-[320px] pt-[30px]"
+                className="pl-[110px] md:pl-[200px] lg:pl-[250px] xl:pl-[320px] pt-[30px]"
             >
 
                 <h3 className={`font-normal text-[35px] xl:text-[40px] leading-[80px] tracking-wide ${isMobile && "h-[160px]"}`}>
