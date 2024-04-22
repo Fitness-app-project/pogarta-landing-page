fa73a5eb11f03fbf58579be71cfeadc6351a8d64
naMqe <namqe07@gmail.com>
2024-04-22 14:03:44 +0200

    fixed screen3 on mobile

---
+++ src/components/Screen3/Content.tsx
@@ -0,0 +1,73 @@
+"use client"
+
+import Image from "next/image"
+import { motion } from 'framer-motion'
+import { TypeAnimation } from 'react-type-animation'
+import { isDesktop, isMobile } from "react-device-detect"
+
+interface IProps {
+    sequence: (string | number)[]
+    text1: string
+    text2: string
+}
+
+export const Content: React.FC<IProps> = ({ sequence, text1, text2 }) => {
+
+    return (
+        <div className={`md:mt-16 xl:mt-32 flex xl:w-[900px] relative ${isDesktop ? "h-[20vh]" : "w-screen h-[400px]"}`}>
+            <motion.div
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ duration: 3 }}
+                viewport={{ once: true }}
+                className='absolute left-[-120px] lg:left-[-80px] xl:left-0'
+            >
+                <Image
+                    alt='Coin'
+                    src="/images/coin1.png"
+                    width={400}
+                    height={400}
+                    className="scale-50 md:scale-75 xl:scale-100"
+                />
+            </motion.div>
+            <motion.div
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ duration: 3 }}
+                viewport={{ once: true }}
+                className="pl-[130px] md:pl-[200px] lg:pl-[250px] xl:pl-[320px] pt-[30px]"
+            >
+
+                <h3 className={`font-normal text-[40px] leading-[80px] tracking-wide ${isMobile && "h-[160px]"}`}>
+                    Building{" "}
+                    <TypeAnimation
+                        sequence={sequence}
+                        wrapper="span"
+                        speed={20}
+                        deletionSpeed={90}
+                        repeat={Infinity}
+                        cursor={false}
+                    />
+                </h3>
+
+                <p className={`font-normal text-[16px] md:text-[22px] leading-[35px] text-[#B7B7B7] pr-4`}>
+                    <TypeAnimation
+                        sequence={[ text1 ]}
+                        wrapper="span"
+                        speed={50}
+                        cursor={false}
+                    />
+
+                    <br />
+                    
+                    <TypeAnimation
+                        sequence={[ text2 ]}
+                        wrapper="span"
+                        speed={50}
+                        cursor={false}
+                    />
+                </p>
+            </motion.div>
+        </div>
+    )
+}
--- src/components/Screen3/Screen3.tsx
+++ src/components/Screen3/Screen3.tsx
@@ -5,11 +5,58 @@ import { useRef } from 'react';
 import { isDesktop, isMobile } from 'react-device-detect';
 // import Typewriter from './components/Typewriter';
 import { TypeAnimation } from 'react-type-animation';
+import { Content } from './Content';
 
 const content = [
     "We build new ventures and non-standard",
     "solutions based on our knowledge and",
     "satisfaction of many customers."
+]
+
+const sequence1 = [
+  "initiatives",
+  750,
+  "startups",
+  750,
+  "innovations",
+  750,
+  "ventures",
+  750,
+  "enterprises",
+  750,
+  "incubations",
+  750,
+  "launches",
+  750,
+  "creations",
+  750,
+  "developments",
+  750,
+  "businesses",
+  750,
+]
+
+const sequence2 = [
+  "enterprises",
+  750,
+  "innovations",
+  750,
+  "solutions",
+  750,
+  "partnerships",
+  750,
+  "collaborations",
+  750,
+  "initiatives",
+  750,
+  "ventures",
+  750,
+  "creations",
+  750,
+  "developments",
+  750,
+  "projects",
+  750,
 ]
 
 const ContentLine = ({ content }: { content: string }) => {
@@ -44,177 +91,37 @@ export const Screen3 = () => {
     return (
       <div className="top-0 z-50">
 
-        <div id="about" className={`w-screen h-[180vh] py-20 px-10 sm:px-12 md:px-24 lg:px-32 xl:px-40 2xl:px-64 screen3-background`} style={{ borderTopLeftRadius: isDesktop ? '3vh' : '0', borderTopRightRadius: isDesktop ? '3vh' : '0' }}>
+        <div id="about" className={`w-screen h-[180vh] py-20 sm:px-12 md:px-24 lg:px-32 xl:px-40 2xl:px-64 screen3-background`} style={{ borderTopLeftRadius: isDesktop ? '3vh' : '0', borderTopRightRadius: isDesktop ? '3vh' : '0' }}>
 
             <div>
-              {content.map((item: string) => (
-                  <ContentLine
-                      key={item}
-                      content={item}
+              {isDesktop ? (
+                content.map((item: string) => (
+                    <ContentLine
+                        key={item}
+                        content={item}
+                    />
+                ))
+              ) : (
+                <div className='w-screen px-5'>
+                  <ContentLine 
+                    key={content[0]}
+                    content={content.join(" ")}
                   />
-              ))}
+                </div>
+              )}
             </div>
-            
-            {/*  */}
-            <div className="md:mt-16 xl:mt-32 flex xl:w-[900px] relative h-[20vh]">
-                <motion.div
-                    initial={{ opacity: 0 }}
-                    whileInView={{ opacity: 1 }}
-                    transition={{ duration: 3 }}
-                    viewport={{ once: true }}
-                    className='absolute left-[-120px] lg:left-[-80px] xl:left-0'
-                >
-                    <Image
-                        alt='Coin'
-                        src="/images/coin1.png"
-                        width={400}
-                        height={400}
-                        className="scale-50 md:scale-75 xl:scale-100"
-                    />
-                </motion.div>
-                <motion.div
-                  initial={{ opacity: 0 }}
-                  whileInView={{ opacity: 1 }}
-                  transition={{ duration: 3 }}
-                  viewport={{ once: true }}
-                  className="pl-[150px] md:pl-[200px] lg:pl-[250px] xl:pl-[320px] pt-[30px]"
-                >
-                  <h3 className="font-normal text-[40px] leading-[80px] tracking-wide">
-                  Building{" "}
-                    <TypeAnimation
-                    sequence={[
-                        "initiatives",
-                        750,
-                        "startups",
-                        750,
-                        "innovations",
-                        750,
-                        "ventures",
-                        750,
-                        "enterprises",
-                        750,
-                        "incubations",
-                        750,
-                        "launches",
-                        750,
-                        "creations",
-                        750,
-                        "developments",
-                        750,
-                        "businesses",
-                        750,
-                    ]} 
-                      wrapper="span"
-                      speed={20}
-                      deletionSpeed={90}
-                      repeat={Infinity}
-                      cursor={false}
-                      
-                    />
-                  </h3>
-                <p className="font-normal text-[16px] md:text-[22px] leading-[35px] text-[#B7B7B7]">
-                    <TypeAnimation
-                      sequence={[
-                        "Crafting Visions into reality.",
-                      ]}
-                      wrapper="span"
-                      speed={50}
-                      cursor={false}
-                    />
-                    <br />
-                    <TypeAnimation
-                      sequence={[
-                        "Our expert team transforms bold ideas into tangible successes, across a spectrum of ventures and innovations.",
-                      ]}
-                      wrapper="span"
-                      speed={50}
-                      cursor={false}
-                    />
-                </p>
-                </motion.div>
-                            </div>
 
+            <Content 
+              sequence={sequence1}
+              text1="Crafting Visions into reality."
+              text2="Our expert team transforms bold ideas into tangible successes, across a spectrum of ventures and innovations."
+            />
 
-              {/*  */}
-              <div className="mt-48 xl:mt-32 flex xl:w-[900px] relative h-[20vh]">
-                  <motion.div
-                      initial={{ opacity: 0 }}
-                      whileInView={{ opacity: 1 }}
-                      transition={{ duration: 3 }}
-                      viewport={{ once: true }}
-                      className='absolute left-[-120px] lg:left-[-80px] xl:left-0'
-                  >
-                      <Image
-                          alt='Coin'
-                          src="/images/coin2.png"
-                          width={400}
-                          height={400}
-                          className="scale-50 md:scale-75 xl:scale-100"
-                      />
-                  </motion.div>
-                  
-                  <motion.div
-                  initial={{ opacity: 0 }}
-                  whileInView={{ opacity: 1 }}
-                  transition={{ duration: 3 }}
-                  viewport={{ once: true }}
-                  className="pl-[150px] md:pl-[200px] lg:pl-[250px] xl:pl-[320px] pt-[30px]"
-                >
-                <h3 className="font-normal text-[40px] leading-[80px] tracking-wide">
-                  Scaling-up{" "}
-                  <TypeAnimation
-                    sequence={[
-                      "enterprises",
-                      750,
-                      "innovations",
-                      750,
-                      "solutions",
-                      750,
-                      "partnerships",
-                      750,
-                      "collaborations",
-                      750,
-                      "initiatives",
-                      750,
-                      "ventures",
-                      750,
-                      "creations",
-                      750,
-                      "developments",
-                      750,
-                      "projects",
-                      750,
-                    ]}
-                    wrapper="span"
-                    speed={20}
-                    deletionSpeed={90}
-                    repeat={Infinity}
-                    cursor={false}
-                  />
-                </h3>
-                <p className="font-normal text-[16px] md:text-[22px] leading-[35px] text-[#B7B7B7]">
-                  <TypeAnimation
-                    sequence={[
-                      "Our expertise from launching and scaling various tech ventures enables us to assist early-stage startups and their founders.",
-                    ]}
-                    wrapper="span"
-                    speed={50}
-                    cursor={false}
-                  />
-                  <br />
-                  <TypeAnimation
-                    sequence={[
-                        7000,
-                      "Collaboratively, we expedite startup growth, propelling them towards international triumph.",
-                    ]}
-                    wrapper="span"
-                    speed={50}
-                    cursor={false}
-                  />
-                </p>
-
-                </motion.div>
-            </div>
+            <Content 
+              sequence={sequence2}            
+              text1='Our expertise from launching and scaling various tech ventures enables us to assist early-stage startups and their founders.'
+              text2='Collaboratively, we expedite startup growth, propelling them towards international triumph.'
+            />
 
             <div className='hidden sm:flex items-center gap-10 sm:mt-48 md:mt-64 lg:mt-32'>
                 <h3 className="font-medium text-[40px] leading-[80px] text-white">Our portfolio in numbers</h3>
